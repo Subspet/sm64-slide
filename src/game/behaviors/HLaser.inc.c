50d253ddc8b4c181bb82b78d22dc7230c07d17d9
Solid-Simp <maliciousrevolver@gmail.com>
2024-04-14 22:28:08 -0400

    all objects placed, potential final build

--- src/game/behaviors/HLaser.inc.c
+++ src/game/behaviors/HLaser.inc.c
@@ -25,13 +25,16 @@ void bhv_HLaser_loop(void) {
         case (0): // stationary
             break;
         case (1): // y axis movement UP/DOWN
-            o->oPosY = o->oHomeY - (0 - (250 * sins(o->oTimer * 600)));
+            o->oPosY = o->oHomeY - (0 - (200 * sins(o->oTimer * 600)));
             break;
-        case (2): // y axis movement UP/DOWN
+        case (2): // y axis movement UP/DOWN farther
             o->oPosY = o->oHomeY - (0 - (400 * -sins(o->oTimer * 600)));
             break;
-        case (3): // y axis movement UP/DOWN
+        case (3): // y axis movement UP/DOWN farther
             o->oPosY = o->oHomeY - (0 - (400 * sins(o->oTimer * 600)));
+            break;
+        case (4): // y axis movement UP/DOWN opp
+            o->oPosY = o->oHomeY - (0 - (200 * -sins(o->oTimer * 600)));
             break;
 		default:
             break;
